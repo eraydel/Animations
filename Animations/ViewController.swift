bec0f5a2d5f67ca7e391d0ce8b6c6dd98991a0df
Erick Ayala <ayala.delgadillo.erick@icloud.com>
2022-05-17 16:04:08 -0500

    Initial Commit

---
+++ Animations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by Erick Ayala Delgadillo on 17/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
